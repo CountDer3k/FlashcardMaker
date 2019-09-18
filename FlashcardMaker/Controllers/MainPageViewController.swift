096e1c6c9d8bdbc0aef08371d7b883ed87b219f3
Der3k <der3kburrola@gmail.com>
2019-09-18 10:24:13 -0600

    Fixed first question showing up as blank Fixed number of questions on top right (now shows correct amount & starts at 0) Added Nutrition 1020 Exam 1 questions Added an intro screen to select exam flash cards and then take you to questions page
    Bugs:
    -Questions page screen is shrunk on top

---
+++ FlashcardMaker/Controllers/MainPageViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MainPageViewController.swift
+//  FlashcardMaker
+//
+//  Created by Derek Burrola on 9/18/19.
+//  Copyright © 2019 Derek Burrola. All rights reserved.
+//
+
+import UIKit
+
+class MainPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    @IBAction func toQuestions(_ sender: Any) {
+        print("going to segue")
+        performSegue(withIdentifier: "toQuestions", sender: self)
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
